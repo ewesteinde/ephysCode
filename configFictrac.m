95978c84650705ec08888c5f6e0ca7339642e098
ewesteinde8320 <ewesteinde@g.harvard.edu>
2024-01-26 15:06:20 -0500

    deleted duplicated files

--- configFictrac.m
+++
@@ -1,8 +0,0 @@
-function configFictrac()
-
-expDir = 'C:\Code\FicTrac\sample';
-FT_PATH = 'C:\Code\FicTrac\bin\Release\configGui.exe';
-cmdStr = ['cd "', expDir, '" & start "" "',  FT_PATH, ...
-        '" config_current.txt & exit'];
-system(cmdStr);
-end
